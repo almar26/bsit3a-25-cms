3dd976ac68fd3b8e17fe4901ce3ac2f71bf8e48c
Almar <gomezalmar26@gmail.com>
2025-06-29 10:15:29 +0800

    Update student info controller

--- src/api/student-info/controllers/student-info.js
+++ src/api/student-info/controllers/student-info.js
@@ -4,6 +4,8 @@
  * student-info controller
  */
 
+// Sample
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::student-info.student-info');
